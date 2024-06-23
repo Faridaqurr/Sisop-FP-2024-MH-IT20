bcb98b14a9b4d2b9150721f45b3cdffb604f906c
Faridaqurr <150933246+Faridaqurr@users.noreply.github.com>
2024-06-23 21:03:32 +0700

    Update discorit.c

--- discorit.c
+++ discorit.c
@@ -6,145 +6,118 @@
 #include <crypt.h>
 #include <errno.h>
 
-#define SERVER_PORT 8080
-#define SERVER_IP "127.0.0.1"  // Pastikan ini mengarah ke IP yang benar
+#define PORT 8080
 
-void register_user(const char *username, const char *password) {
-    int sock;
-    struct sockaddr_in server_addr;
-    char buffer[1024];
-
-    // Buat socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
-        exit(EXIT_FAILURE);
-    }
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-
-    // Konversi alamat IP dari text ke binary
-    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
-        perror("Invalid address/Address not supported");
-        exit(EXIT_FAILURE);
-    }
-
-    // Hubungkan ke server
-    printf("Connecting to server at %s:%d...\n", SERVER_IP, SERVER_PORT);
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connection Failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Connected to server\n");
-
-    // Enkripsi password
-    char salt[] = "$6$rounds=5000$abcdefghijk$";  // Ganti salt sesuai kebutuhan
-    char *hashed_password = crypt(password, salt);
-
-    // Kirim request register ke server
-    snprintf(buffer, sizeof(buffer), "REGISTER %s %s", username, hashed_password);
-    send(sock, buffer, strlen(buffer), 0);
-    printf("Sent: %s\n", buffer);
-
-    // Terima response dari server
-    int valread = read(sock, buffer, sizeof(buffer) - 1);
-    buffer[valread] = '\0';
-    printf("Received: %s\n", buffer);
-
-    close(sock);
-}
-
-void login_user(const char *username, const char *password) {
-    int sock;
-    struct sockaddr_in server_addr;
-    char buffer[1024];
-
-    // Buat socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
-        exit(EXIT_FAILURE);
-    }
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-
-    // Konversi alamat IP dari text ke binary
-    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
-        perror("Invalid address/Address not supported");
-        exit(EXIT_FAILURE);
-    }
-
-    // Hubungkan ke server
-    printf("Connecting to server at %s:%d...\n", SERVER_IP, SERVER_PORT);
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connection Failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Connected to server\n");
-
-    // Enkripsi password
-    char salt[] = "$6$rounds=5000$abcdefghijk$";  // Ganti salt sesuai kebutuhan
-    char *hashed_password = crypt(password, salt);
-
-    // Kirim request login ke server
-    snprintf(buffer, sizeof(buffer), "LOGIN %s %s", username, hashed_password);
-    send(sock, buffer, strlen(buffer), 0);
-    printf("Sent: %s\n", buffer);
-
-    // Terima response dari server
-    int valread = read(sock, buffer, sizeof(buffer) - 1);
-    buffer[valread] = '\0';
-    printf("Received: %s\n", buffer);
-
-    close(sock);
-}
-
-//FUNGSI JOIN BELUM AKU COBA
-//FUNGSI JOIN BELUM AKU COBA
-void send_join_command(int sockfd, char *username, char *channel, char *key) {
-    char command[MAX_COMMAND_SIZE];
-    if (key == NULL) {
-        snprintf(command, sizeof(command), "JOIN %s", channel);
-    } else {
-        snprintf(command, sizeof(command), "JOIN %s %s", channel, key);
-    }
-
-    send_message(sockfd, username, command);
-}
-
-// Fungsi untuk menangani perintah JOIN dari pengguna
-void handle_join_command(int sockfd, char *username) {
-    char channel[MAX_CHANNEL_NAME_SIZE];
-    char key[MAX_KEY_SIZE];
-
-    printf("[%s] JOIN channel\n", username);
-    printf("Channel: ");
-    scanf("%s", channel);
-
-    // Cek apakah channel memiliki key atau tidak
-    printf("Key (leave blank if none): ");
-    scanf("%s", key);
-    if (strlen(key) == 0) {
-        key = NULL;
-    }
-
-    send_join_command(sockfd, username, channel, key);
-}
+void handle_response(int socket);
 
 int main(int argc, char const *argv[]) {
-    if (argc < 4) {
-        fprintf(stderr, "Usage: %s <REGISTER|LOGIN> <username> -p <password>\n", argv[0]);
-        return EXIT_FAILURE;
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <COMMAND> <ARGS>\n", argv[0]);
+        return 1;
     }
 
-    if (strcmp(argv[1], "REGISTER") == 0 && strcmp(argv[3], "-p") == 0) {
-        register_user(argv[2], argv[4]);
-    } else if (strcmp(argv[1], "LOGIN") == 0 && strcmp(argv[3], "-p") == 0) {
-        login_user(argv[2], argv[4]);
+    const char *command = argv[1];
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    char message[1024] = {0};
+
+    if (strcmp(command, "REGISTER") == 0) {
+        if (argc < 5) {
+            fprintf(stderr, "Usage: %s REGISTER <USERNAME> -p <PASSWORD>\n", argv[0]);
+            return 1;
+        }
+        const char *username = argv[2];
+        const char *password_flag = argv[3];
+        const char *password = argv[4];
+
+        if (strcmp(password_flag, "-p") != 0) {
+            fprintf(stderr, "Invalid usage. Use -p for password.\n");
+            return 1;
+        }
+
+        char *hashed_password = crypt(password, "$6$abcdefghijk$");
+        snprintf(message, sizeof(message), "%s %s %s", command, username, hashed_password);
+
+    } else if (strcmp(command, "LOGIN") == 0) {
+        if (argc < 5) {
+            fprintf(stderr, "Usage: %s LOGIN <USERNAME> -p <PASSWORD>\n", argv[0]);
+            return 1;
+        }
+        const char *username = argv[2];
+        const char *password_flag = argv[3];
+        const char *password = argv[4];
+
+        if (strcmp(password_flag, "-p") != 0) {
+            fprintf(stderr, "Invalid usage. Use -p for password.\n");
+            return 1;
+        }
+
+        char *hashed_password = crypt(password, "$6$abcdefghijk$");
+        snprintf(message, sizeof(message), "%s %s %s", command, username, hashed_password);
+
+    } else if (strcmp(command, "LIST") == 0) {
+        if (argc < 3) {
+            fprintf(stderr, "Usage: %s LIST <CHANNEL|ROOM|USER> [CHANNEL_NAME]\n", argv[0]);
+            return 1;
+        }
+        const char *subcommand = argv[2];
+        if (strcmp(subcommand, "CHANNEL") == 0) {
+            snprintf(message, sizeof(message), "%s CHANNEL", command);
+        } else if (strcmp(subcommand, "ROOM") == 0 && argc == 4) {
+            const char *channel_name = argv[3];
+            snprintf(message, sizeof(message), "%s ROOM %s", command, channel_name);
+        } else if (strcmp(subcommand, "USER") == 0 && argc == 4) {
+            const char *channel_name = argv[3];
+            snprintf(message, sizeof(message), "%s USER %s", command, channel_name);
+        } else {
+            fprintf(stderr, "Invalid LIST command usage\n");
+            return 1;
+        }
+
+    } else if (strcmp(command, "JOIN") == 0) {
+        if (argc < 4) {
+            fprintf(stderr, "Usage: %s JOIN <CHANNEL|ROOM> <NAME>\n", argv[0]);
+            return 1;
+        }
+        const char *subcommand = argv[2];
+        const char *name = argv[3];
+        snprintf(message, sizeof(message), "%s %s %s", command, subcommand, name);
+
     } else {
-        fprintf(stderr, "Invalid command\n");
-        return EXIT_FAILURE;
+        fprintf(stderr, "Unknown command\n");
+        return 1;
     }
 
+    send(sock, message, strlen(message), 0);
+    handle_response(sock);
+
+    close(sock);
     return 0;
+}
+
+void handle_response(int socket) {
+    char buffer[1024] = {0};
+    int valread = read(socket, buffer, 1024);
+    buffer[valread] = '\0';
+    printf("%s", buffer);
 }
